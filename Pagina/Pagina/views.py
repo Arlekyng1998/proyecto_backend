4ae2cdd5ed4e224aae6fd96aae6b413f6c09eee7
Arlekyng1998 <jhon@localhost.localdomain>
2020-06-15 00:53:10 -0500

    hola gg

---
+++ Pagina/Pagina/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+def saludo(request): #Primera vista
+    return HttpResponse("hola que mas")
+
+def salida(request):
+    return HttpResponse("bey gays")
+
+def html(request):
+    documento="""<html>
+    <body>
+    <h1>ahorasi</h1>
+    <body>
+    </html>
+    """
+    return HttpResponse(documento)
