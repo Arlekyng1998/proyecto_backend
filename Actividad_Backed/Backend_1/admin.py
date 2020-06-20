1414a165721a233a561e1dbb035c3106624b19f9
Arlekyng1998 <jhon@localhost.localdomain>
2020-06-20 07:34:43 -0500

    depuración

--- Actividad_Backed/Backend_1/admin.py
+++ Actividad_Backed/Backend_1/admin.py
@@ -8,18 +8,18 @@ from Backend_1.models import *
 
 class clientesAdmin(admin.ModelAdmin):
     list_display = ("nombre", "apellido", "numero_cc", "correo", "id_compra")
-    search_file = ("id_compra",)
+    search_file = ("id_compra", "numero_cc", "id_compra")
     list_filter = ("id_compra",)
 
 class ventasAdmin(admin.ModelAdmin):
     list_display = ("id_producto", "cantidad", "vendedor")
-    search_file = ("id_producto",)
+    search_file = ("id_producto", "vendedor")
     list_filter = ("id_producto",)
     date_hierarchy = "fecha"
 
 class inventarioAdmin(admin.ModelAdmin):
     list_display = ("producto", "cantidad_bodega", "valor_bodega")
-    search_fields = ("producto",)
+    search_fields = ("producto", "cantidad_bodega")
     list_filter = ("producto", "cantidad_bodega")
     date_hierarchy = "fecha_entrega"
 
